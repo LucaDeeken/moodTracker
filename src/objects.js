d2af0bafc28adb851904e6e98c4fd1d1f3789597
Luca Deeken <LDeeken@aol.com>
2025-02-02 16:52:01 +0100

    aadd class constructeur

---
+++ src/objects.js
@@ -0,0 +1,27 @@
+export class moodTracker {
+  constructor(
+    terror = false,
+    depressed = false,
+    veryBad = false,
+    bad = false,
+    discontent = false,
+    medium = false,
+    okay = false,
+    fine = false,
+    good = false,
+    veryGood = false,
+    godlike = false,
+  ) {
+    this.terror = false;
+    this.depressed = false;
+    this.veryBad = false;
+    this.bad = false;
+    this.discontent = false;
+    this.medium = false;
+    this.okay = false;
+    this.fine = false;
+    this.good = false;
+    this.veryGood = false;
+    this.godlike = false;
+  }
+}
