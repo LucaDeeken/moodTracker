02523d4da6174453b725d2208a8c3138ab1e5ffc
Luca Deeken <LDeeken@aol.com>
2025-10-11 20:54:27 +0200

    fix: editButton bug; change gridColors.

--- src/today.js
+++ src/today.js
@@ -36,6 +36,7 @@ preloadImages(moodImages);
 
 let alreadyClickedSubmit = false;
 let doneIcon = "";
+let mood ="";
 export function DOMtoday() {
   document
     .getElementById("checkBoxes")
@@ -49,7 +50,7 @@ export function DOMtoday() {
       );
       newHash.set(selectedBox.value);
       pulseCheckbox(selectedBox.value);
-      const mood = textByMood(selectedBox.value);
+      mood = textByMood(selectedBox.value);
 
       const submitButton = document.getElementById("submitBtn");
       const checkBox = document.getElementById("checkBoxes");
@@ -72,144 +73,10 @@ export function DOMtoday() {
         doneIcon.style.display = "block";
         doneIcon.style.opacity = 1;
 
-        
-
-
-
         submitButton.addEventListener("click", () => {
-        const mainArea = document.querySelector(".mainArea");
-        mainArea.innerHTML = "";
-        mainArea.id = "";
-        mainArea.innerHTML = `     <div id="textContainerToday">
-        <p id="textToday">How are you feeling today, Luca?</p>
-      </div>
-      <img src="./images/done.png" alt="Done-Icon" class="doneImage">
-      <form id="checkBoxes">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          id="sad"
-          viewBox="0 0 24 24"
-          height="35px"
-        >
-          <path
-            d="M8 11V9a1 1 0 0 1 2 0v2a1 1 0 0 1-2 0zm7 1a1 1 0 0 0 1-1V9a1 1 0 0 0-2 0v2a1 1 0 0 0 1 1zm-3 2a6.036 6.036 0 0 0-4.775 2.368 1 1 0 1 0 1.55 1.264 4 4 0 0 1 6.45 0 1 1 0 0 0 1.55-1.264A6.036 6.036 0 0 0 12 14zm11-2A11 11 0 1 1 12 1a11.013 11.013 0 0 1 11 11zm-2 0a9 9 0 1 0-9 9 9.01 9.01 0 0 0 9-9z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          id="happy"
-          viewBox="0 0 24 24"
-          height="35px"
-        >
-          <path
-            d="M12 1a11 11 0 1 0 11 11A11.013 11.013 0 0 0 12 1zm0 20a9 9 0 1 1 9-9 9.011 9.011 0 0 1-9 9zm6-8a6 6 0 0 1-12 0 1 1 0 0 1 2 0 4 4 0 0 0 8 0 1 1 0 0 1 2 0zM8 10V9a1 1 0 0 1 2 0v1a1 1 0 0 1-2 0zm6 0V9a1 1 0 0 1 2 0v1a1 1 0 0 1-2 0z"
-          />
-        </svg>
-        <div id="one">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="terror"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="two">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="depressed"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="three">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="veryBad"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="four">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="bad"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="five">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="discontent"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="six">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="okay"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="seven">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="fine"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="eight">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="good"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="nine">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="veryGood"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="ten">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="godlike"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-      </form>
-      <div id="btnContainer">
-        <button type="submit" form="checkBoxes" id="submitBtn">Submit</button>
-      </div>`;
-        DOMtoday();
+          alreadyClickedSubmit = false;
+          loadTodayHTMLNew();
+          DOMtoday();
       });
 
 
@@ -304,139 +171,28 @@ export function getTodayHTML() {
     mainArea.innerHTML = "";
     mainArea.id = "";
     if (!alreadyClickedSubmit) {
-      mainArea.innerHTML = `     <div id="textContainerToday">
-        <p id="textToday">How are you feeling today, Luca?</p>
-      </div>
-      <img src="./images/done.png" alt="Done-Icon" class="doneImage">
-      <form id="checkBoxes">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          id="sad"
-          viewBox="0 0 24 24"
-          height="35px"
-        >
-          <path
-            d="M8 11V9a1 1 0 0 1 2 0v2a1 1 0 0 1-2 0zm7 1a1 1 0 0 0 1-1V9a1 1 0 0 0-2 0v2a1 1 0 0 0 1 1zm-3 2a6.036 6.036 0 0 0-4.775 2.368 1 1 0 1 0 1.55 1.264 4 4 0 0 1 6.45 0 1 1 0 0 0 1.55-1.264A6.036 6.036 0 0 0 12 14zm11-2A11 11 0 1 1 12 1a11.013 11.013 0 0 1 11 11zm-2 0a9 9 0 1 0-9 9 9.01 9.01 0 0 0 9-9z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          id="happy"
-          viewBox="0 0 24 24"
-          height="35px"
-        >
-          <path
-            d="M12 1a11 11 0 1 0 11 11A11.013 11.013 0 0 0 12 1zm0 20a9 9 0 1 1 9-9 9.011 9.011 0 0 1-9 9zm6-8a6 6 0 0 1-12 0 1 1 0 0 1 2 0 4 4 0 0 0 8 0 1 1 0 0 1 2 0zM8 10V9a1 1 0 0 1 2 0v1a1 1 0 0 1-2 0zm6 0V9a1 1 0 0 1 2 0v1a1 1 0 0 1-2 0z"
-          />
-        </svg>
-        <div id="one">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="terror"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="two">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="depressed"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="three">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="veryBad"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="four">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="bad"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="five">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="discontent"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="six">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="okay"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="seven">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="fine"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="eight">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="good"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="nine">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="veryGood"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-        <div id="ten">
-          <label>
-            <input
-              type="checkbox"
-              name="option1"
-              value="godlike"
-              class="inputCheckBox"
-            />
-          </label>
-        </div>
-      </form>
-      <div id="btnContainer">
-        <button type="submit" form="checkBoxes" id="submitBtn">Submit</button>
-      </div>`;
+      loadTodayHTMLNew();
     } else {
-      mainArea.innerHTML = `
-        <div id="textContainerToday">
-          <p id="textToday" class="">Great to see you in a good place!</p>
+      loadTodayHTMLDone();
+      const submitBtn = document.getElementById("submitBtn");
+      submitBtn.addEventListener("click", () => {
+        loadTodayHTMLNew();
+        DOMtoday();
+      });
+    }
+
+    mainArea.id = "mainAreaToday";
+    DOMtoday();
+    mainAreaStats.style.transform = "";
+  }, 600);
+}
+
+function loadTodayHTMLDone() {
+        const mainArea = document.querySelector(".mainArea");
+        mainArea.innerHTML = "";
+        mainArea.id = "";
+        mainArea.innerHTML = `     <div id="textContainerToday">
+          <p id="textToday" class="">${mood}</p>
         </div>
         <img src=${doneIcon.src} alt="Done-Icon" class="doneImage" style="display: block; opacity: 1;">
         <form id="checkBoxes" class="bg-farbe-9 active" style="--pulse-color: #32CD32; display: none;">
@@ -500,8 +256,10 @@ export function getTodayHTML() {
         <div id="btnContainer">
           <button type="submit" form="checkBoxes" id="submitBtn">Edit</button>
         </div>`;
-      const submitBtn = document.getElementById("submitBtn");
-      submitBtn.addEventListener("click", () => {
+}
+
+function loadTodayHTMLNew() {
+  const mainArea = document.querySelector(".mainArea");
         mainArea.innerHTML = `     <div id="textContainerToday">
         <p id="textToday">How are you feeling today, Luca?</p>
       </div>
@@ -631,12 +389,4 @@ export function getTodayHTML() {
       <div id="btnContainer">
         <button type="submit" form="checkBoxes" id="submitBtn">Submit</button>
       </div>`;
-        DOMtoday();
-      });
-    }
-
-    mainArea.id = "mainAreaToday";
-    DOMtoday();
-    mainAreaStats.style.transform = "";
-  }, 600);
 }
